67f2bfc61c0aa165af94cede07e54102147a432e
wicky4t <bhaiwicky5@gmail.com>
2025-08-31 14:42:32 +0530

    Updated VideoThumbnail.tsx

--- src/components/VideoThumbnail.tsx
+++ src/components/VideoThumbnail.tsx
@@ -112,7 +112,7 @@ export function VideoThumbnail({
       {/* Placeholder while not in view */}
       {!isInView && (
         <div className="w-full h-full bg-gradient-to-br from-gray-800 to-gray-900 flex items-center justify-center">
-          <Play className="w-8 h-8 text-white/30" />
+          <Play className="w-8 h-8 text-white/0" />
         </div>
       )} 
 
