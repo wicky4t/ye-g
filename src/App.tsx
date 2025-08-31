a87b3c411861e8b55101a8e497560eda742efb20
wicky4t <bhaiwicky5@gmail.com>
2025-08-31 14:15:04 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -326,8 +326,7 @@ gsap.to(backgroundTextRef.current, {
         <div className="relative">
           <div
             className="
-              w-[35rem] h-[35rem]
-              sm:w-[60px] sm:h-[600px]
+              w-[35rem] h-[5rem]
               lg:w-[62.5rem] lg:h-[62.5rem]
               overflow-hidden
             "
