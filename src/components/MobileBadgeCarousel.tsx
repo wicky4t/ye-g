2f64e70497c0252fedfc3786c6396834ac6a4b12
wicky4t <bhaiwicky5@gmail.com>
2025-08-31 13:59:43 +0530

    Updated MobileBadgeCarousel.tsx

--- src/components/MobileBadgeCarousel.tsx
+++ src/components/MobileBadgeCarousel.tsx
@@ -44,7 +44,7 @@ export function MobileBadgeCarousel() {
 
   return (
     <div className="fixed top-4 left-1/2 transform -translate-x-1/2 z-20 md:hidden">
-      <div className="relative w-300 h-300">
+      <div className="relative w-1000 h-1000">
         {/* Base Badge PNG */}
         <img
           src={currentBadge.image}
