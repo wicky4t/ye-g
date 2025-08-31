96911340d0f9f68cbdcf7155962b9504e71cce0a
wicky4t <bhaiwicky5@gmail.com>
2025-08-31 14:24:44 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -283,10 +283,12 @@ gsap.to(backgroundTextRef.current, {
          {/* Base */}
       <div
         ref={baseRef}
-        className={`fixed inset-0 flex items-center justify-center z-20 transition-opacity duration-100`}
-        style={{
-          top: "24%",
-          left: "1%",
+        className={`
+    fixed flex items-center justify-center z-40 transition-opacity duration-100
+    top-[20%] left-1/2 -translate-x-1/2   /* Mobile (default) */
+    md:top-[24%] md:left-[1%] md:translate-x-0 /* PC/Tablet */
+  `}
+  style={{
           opacity: showbase ? 1 : 0,
           pointerEvents: showbase ? "auto" : "none",
         }}
@@ -294,9 +296,7 @@ gsap.to(backgroundTextRef.current, {
         <div className="relative">
           <div
             className="
-              w-[35rem] h-[35rem]
-              sm:w-[600px] sm:h-[600px]
-              md:w-[50rem] md:h-[50rem]
+              w-[40rem] h-[40rem]
               lg:w-[62.5rem] lg:h-[62.5rem]
               overflow-hidden
             "
